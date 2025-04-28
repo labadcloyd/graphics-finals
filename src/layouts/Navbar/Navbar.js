602d1fe53a3eedf64373845056d5d927a655cdd8
Cloyd Abad <abadcloyd@gmail.com>
2025-04-28 16:51:50 +0800

    added navbar

---
+++ src/layouts/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+export default function Navbar() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div>
+          <h4>Midpoint Ellipse</h4>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
